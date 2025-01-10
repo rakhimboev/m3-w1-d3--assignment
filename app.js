e50694383ee061a839169e85988820e606c76d2b
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2025-01-09 23:32:13 -0500

    week 1 lab

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/index");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", routes);
+
+module.exports = app;
